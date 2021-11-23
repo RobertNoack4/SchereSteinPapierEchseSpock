09434c3d44e5d354cab7c4cdd0627c9877345d2b
robert <robertnoack4@gmail.com>
2021-11-23 07:30:11 +0100

    Menueauswahl

--- SchereSteinPapierEchseSpock/Program.cs
+++ SchereSteinPapierEchseSpock/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace SchereSteinPapierEchseSpock
+﻿using System;
+
+namespace SchereSteinPapierEchseSpock
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Bitte wählen sie eine Option: ");
+            Console.WriteLine("1 = Schere");
+            Console.WriteLine("2 = Stein");
+            Console.WriteLine("3 = Papier");
+            Console.WriteLine("4 = Echse");
+            Console.WriteLine("5 = Spock");
+            Console.WriteLine("Geben sie eine Zahl zwischen 1 und 5 ein!");
         }
     }
 }
